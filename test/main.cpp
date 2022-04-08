e148aa44693771ec4b11a48a0fb7f63d41eef8f4
mutsuki <mutsuki@momo86.net>
2022-04-08 23:14:31 +0900

    Update test

--- test/main.cpp
+++ test/main.cpp
@@ -1,17 +1,21 @@
 #include <print_table.hpp>
 
 int main() {
-	mtk::print_table::table_t table;
+	std::vector<std::map<std::string, std::string>> table;
 
 	for (unsigned i = 0; i < 10; i++) {
-		mtk::print_table::row_t row;
+		std::map<std::string, std::string> row;
 		row.insert(std::make_pair("N"   , std::to_string(i)));
 		row.insert(std::make_pair("N^2" , std::to_string(i * i)));
 		row.insert(std::make_pair("test", "TEST-" + std::to_string(i * i * i * i)));
 		table.push_back(row);
 	}
 
-	std::vector<std::string> col_names = {"N", "N^2", "test"};
+	std::printf("--------------------------------------\n");
+	std::vector<std::string> cols = {"N", "N^2", "test"};
+	mtk::print_table::print_table(table, cols);
 
-	mtk::print_table::print_table(table, col_names);
+	std::printf("--------------------------------------\n");
+	std::vector<std::string> col_names = {"n", "n^2", "TEST"};
+	mtk::print_table::print_table(table, cols, col_names);
 }
