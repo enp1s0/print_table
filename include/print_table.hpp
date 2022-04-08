b6686ed8ea23c5fb88c7bb621893762a72654ddd
mutsuki <mutsuki@momo86.net>
2022-04-08 23:14:21 +0900

    Add changinf col names

--- include/print_table.hpp
+++ include/print_table.hpp
@@ -50,56 +50,59 @@ inline void print_row(
 }
 
 inline void print_row(
+		std::vector<std::string> cols,
 		std::vector<std::string> col_names,
 		std::unordered_map<std::string, std::size_t> max_col_name_length,
 		const unsigned padding,
 		FILE* const output) {
 	// Make a col name table row to input to `print_row` above.
 	std::unordered_map<std::string, std::string> col_names_row;
-	for (const auto& col_name : col_names) {
-		col_names_row.insert(std::make_pair(col_name, col_name));
+	for (unsigned i = 0; i < cols.size(); i++) {
+		col_names_row.insert(std::make_pair(cols[i], col_names[i]));
 	}
 
-	print_row(col_names, col_names_row, max_col_name_length, padding, output);
+	print_row(cols, col_names_row, max_col_name_length, padding, output);
 }
 
 } // namespace detail
 
 inline void print_table(
 		const table_t table,
+		const std::vector<std::string> cols,
 		const std::vector<std::string> col_names,
 		FILE* output = stdout
 		) {
 	// Calculate the each column width
 	std::unordered_map<std::string, std::size_t> max_col_name_length;
-	for (const auto &col_name : col_names) {
-		max_col_name_length.insert(std::make_pair(col_name, col_name.length()));
+	for (unsigned i = 0; i < col_names.size(); i++) {
+		max_col_name_length.insert(std::make_pair(cols[i], col_names[i].length()));
 	}
 
 	for (const auto &row : table) {
-		for (const auto &col_name : col_names) {
+		for (const auto &col_name : cols) {
 			max_col_name_length[col_name] = std::max(max_col_name_length[col_name], row.at(col_name).length());
 		}
 	}
 
 	constexpr auto padding = 1;
 	unsigned table_width = 1;
-	for (const auto& col_name : col_names) {
+	for (const auto& col_name : cols) {
 		table_width += (max_col_name_length[col_name] + padding * 2 + 1);
 	}
 
 	// Output col names
-	detail::print_hline(col_names, max_col_name_length, padding, output);
-	detail::print_row(col_names, max_col_name_length, padding, output);
-	detail::print_hline(col_names, max_col_name_length, padding, output);
+	detail::print_hline(cols, max_col_name_length, padding, output);
+	detail::print_row(cols, col_names, max_col_name_length, padding, output);
+	detail::print_hline(cols, max_col_name_length, padding, output);
 	for (const auto &row : table) {
-		detail::print_row(col_names, row, max_col_name_length, padding, output);
+		detail::print_row(cols, row, max_col_name_length, padding, output);
 	}
-	detail::print_hline(col_names, max_col_name_length, padding, output);
+	detail::print_hline(cols, max_col_name_length, padding, output);
 }
 
 inline void print_table(
 		const std::vector<std::map<std::string, std::string>>& table,
+		const std::vector<std::string> cols,
 		const std::vector<std::string> col_names,
 		FILE* output = stdout
 		) {
@@ -112,7 +115,16 @@ inline void print_table(
 		t.push_back(r);
 	}
 	
-	print_table(t, col_names, output);
+	print_table(t, cols, col_names, output);
+}
+
+template <class TABLE_T>
+inline void print_table(
+		const TABLE_T& table,
+		const std::vector<std::string> cols,
+		FILE* output = stdout
+		) {
+	print_table(table, cols, cols, output);
 }
 } // namespace print_table
 } // namespace mtk
